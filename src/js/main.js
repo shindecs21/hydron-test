ba1b8a57202a1774c888db6c7e71921d19224c4e
Chetan Shinde <14790035+shindecs21@users.noreply.github.com>
2024-07-14 11:02:08 +0530

    index.js js renamed to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@
 import '../scss/style.scss';
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
-import bsUtil from 'bootstrap/js/dist/util/index.js';
+import * as bootstrap from '../../node_modules/bootstrap';
+import bsUtil from '../../node_modules/bootstrap/js/dist/util/index.js';
 
 console.log('startted my firs js line !!!!!!!!');
 console.log(' test getUID' + bsUtil.getUID('2ss'));
