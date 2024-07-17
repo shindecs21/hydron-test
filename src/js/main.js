62dd576cdee648935a8db3aca4a0d9c3350ceba7
Chetan Shinde <14790035+shindecs21@users.noreply.github.com>
2024-07-18 02:06:33 +0530

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 // Import our custom CSS
-import '../scss/style.scss';
+import "../scss/style.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from '../../node_modules/bootstrap';
-import bsUtil from '../../node_modules/bootstrap/js/dist/util/index.js';
 
-console.log('startted my firs js line !!!!!!!!');
-console.log(' test getUID' + bsUtil.getUID('2ss'));
+import * as bootstrap from "../../node_modules/bootstrap";
+import bsUtil from "../../node_modules/bootstrap/js/dist/util/index.js";
+
+console.log("startted my firs js line !!!!!!!!");
+console.log(" test getUID" + bsUtil.getUID("2ss"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,55 @@
-const path = require('path');
+const path = require("path");
 //const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
-
+const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        filename: 'main.js'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(scss)$/,
-                use: ['css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(jpg|png|svg|jpeg)$/,
-                type: 'asset/resource'
-            },
-        ]
-    },
-    plugins: [
-        /* new HtmlWebpackPlugin({
+  mode: "development",
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    filename: "main.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: ["css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|jpeg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    /* new HtmlWebpackPlugin({
             template: 'src/index.html'
         }) */
-        new HtmlBundlerPlugin({
-            entry: {
-                index: 'src/index.html',    // define templates here
-            },
-            js: {
-                filename: 'js/[name].[contenthash:8].js',   // output filename of compiled JavaScript
-            },
-            css: {
-                filename: 'css/[name].[contenthash:8].css',  // output filename of extracted CSS
-            },
-        }),
-    ],
-    devServer: {
-        open: true,
-        hot: true,
-        static: path.resolve(__dirname, "dist"),
-        watchFiles: ['src/**/*.*'],
-        client: {
-            overlay: {
-                errors: true,
-                warnings: false,
-                runtimeErrors: true,
-            },
-        },
-    }
-}
+    new HtmlBundlerPlugin({
+      entry: {
+        index: "src/index.html", // define templates here
+      },
+      js: {
+        filename: "js/[name].[contenthash:8].js", // output filename of compiled JavaScript
+      },
+      css: {
+        filename: "css/[name].[contenthash:8].css", // output filename of extracted CSS
+      },
+    }),
+  ],
+  devServer: {
+    open: true,
+    hot: true,
+    static: path.resolve(__dirname, "dist"),
+    watchFiles: ["src/**/*.*"],
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+        runtimeErrors: true,
+      },
+    },
+  },
+};
