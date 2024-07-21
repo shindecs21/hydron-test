21d6ef60b20690ba9f0f101d5257fcd233d10039
Chetan Shinde <14790035+shindecs21@users.noreply.github.com>
2024-07-22 00:33:33 +0530

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,11 @@ import bsUtil from "../../node_modules/bootstrap/js/dist/util/index.js";
 
 console.log("startted my firs js line !!!!!!!!");
 console.log(" test getUID" + bsUtil.getUID("2ss"));
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  console.log("adddding even tlisrtner!!!!!!!!!" + window.scrollY);
+  if (window.scrollY > 30) navBar.classList.add("sticky-nav");
+  else navBar.classList.remove("sticky-nav");
+});
